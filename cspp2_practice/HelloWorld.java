95a215a6e70dfb1cee5f581525f8ad3761ab4c39
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-27 18:40:27 +0530

    HelloWorld

---
+++ cspp2_practice/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ *@author Prasanth!!!
+*/
+public final class HelloWorld {
+    /**
+     * Empty Constructor.
+    */
+    private HelloWorld() {
+        //Iam not using this constructor
+    }
+    /**
+     * Main function to print.
+     * @param args is the parameter for this function
+    */
+    public static void main(final String[] args) {
+        System.out.print("Hello Java this is --->Prashu!!");
+    }
+}
