811ad8d14c50e6197bead3e95cd0ba6fd4d347a1
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-27 19:50:38 +0530

    Name

---
+++ cspp2_assignments/M1/Assignment-1/Solution.java
@@ -0,0 +1,20 @@
+/**
+ *@author Prasanth!!!
+*/
+public final class Solution {
+    /**
+     * Empty Constructor.
+    */
+    private Solution() {
+        //Iam not using this constructor
+    }
+    /**
+     * Main function to print.
+     * @param args is the parameter for this function
+    */
+    public static void main(final String[] args) {
+        String name = "Prasanth";
+        long num = 20186080;
+        System.out.println("Name : " + name + " Roll Number :" + num);
+    }
+}
