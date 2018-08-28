44cdf981c49476c78628b3aafb1fd8a6853f3e75
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-28 15:59:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2_assignments/M2/Assignment-2/Solution.java
+++ cspp2_assignments/M2/Assignment-2/Solution.java
@@ -1,16 +1,43 @@
 import java.util.Scanner;
-
+import java.lang.Math;
+/**
+ * @author Prasanth!!
+*/
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
+    /*
+    Do not modify this main function.
+    */
+      /**
+     * Empty Constructor.
+    */
+    private Solution() {
+        //Iam not using this constructor
+    }
+    /**
+     * Main function to print.
+     * @param args is the parameter for this function
+    */
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		// rootsOfQuadraticEquation(a, b, c);
-		System.out.println(123);
+		rootsOfQuadraticEquation(a, b, c);
+	}
+
+	/**
+	 * { function_description }.
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 * @param      c     { parameter_description }
+	 */
+	public static void rootsOfQuadraticEquation(int a, int b, int c) {
+		int var1 = (b * b) - (4 * a * c);
+		double root1 = ((-b) + Math.sqrt(var1)) / (2 * a);
+		double root2 = ((-b) - Math.sqrt(var1)) / (2 * a);
+		System.out.println(root1 + " " + root2);
 	}
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
