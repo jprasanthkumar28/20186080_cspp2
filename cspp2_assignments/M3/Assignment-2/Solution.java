d3e792983f9d0b0361c0a061fa119f8a3e59eee1
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-29 14:59:23 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/M3/Assignment-2/Solution.java
+++ cspp2_assignments/M3/Assignment-2/Solution.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 /**
- * @author Prasanth...
-*/
+ * @author     Prasanth...
+ */
 public final class Solution {
     /**
      * Empty Constructor.
     */
     private static final int TEN = 10;
+
     private static final int SEVEN = 7;
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //Iam not using this constructor.
     }
@@ -21,7 +25,7 @@ public final class Solution {
 
         Scanner s = new Scanner(System.in);      
         int n = s.nextInt();
-        int i = 0,count = 0,value = 0;
+        int i = 0, count = 0, value = 0;
   	    while (i < n) {
 	       if (i % TEN == SEVEN) {
 	       	count += 1;
@@ -32,17 +36,18 @@ public final class Solution {
 		for (int j = 0; j < n; j++) {
 			// System.out.println(j);
 			int a = j / TEN;
-			while(a > 0) {
+			while (a > 0) {
 	     	  if (a % TEN == SEVEN) {
 	       		value += 1;
-	 			}
-	   				a = a / TEN;
+	 		}
+	   			a = a / TEN;
 			}
 			// System.out.println(n);
-			if (a == SEVEN) {
+			if (a == SEVEN){
 				value += 1;
 				// System.out.println(value);
 			}
+
 		}
 		int result = count + value ;
         System.out.println(result);
