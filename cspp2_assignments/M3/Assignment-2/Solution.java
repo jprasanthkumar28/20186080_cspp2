69756d8aa54d7699b0f33c1706ab08bee5d0e4f1
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-29 15:05:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2_assignments/M3/Assignment-2/Solution.java
+++ cspp2_assignments/M3/Assignment-2/Solution.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public final class Solution {
     /**
      * Empty Constructor.
-    */
+     */
     private static final int TEN = 10;
 
     private static final int SEVEN = 7;
@@ -19,37 +19,37 @@ public final class Solution {
      * Main function to print.
      *
      * @param      args  is the parameter for this function
-     */	
+     */
 /* Fill the main function to print the number of 7's between 1 to n*/
     public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);      
         int n = s.nextInt();
         int i = 0, count = 0, value = 0;
-  	    while (i < n) {
-	       if (i % TEN == SEVEN) {
-	       	count += 1;
-	       }
-	   		i += 1;
-		}
-  		// System.out.println(count);
-		for (int j = 0; j < n; j++) {
-			// System.out.println(j);
-			int a = j / TEN;
-			while (a > 0) {
-	     	  if (a % TEN == SEVEN) {
-	       		value += 1;
-	 		}
-	   			a = a / TEN;
-			}
-			// System.out.println(n);
-			if (a == SEVEN){
-				value += 1;
-				// System.out.println(value);
-			}
+        while (i < n) {
+           if (i % TEN == SEVEN) {
+            count += 1;
+           }
+            i += 1;
+        }
+        // System.out.println(count);
+        for (int j = 0; j < n; j++) {
+            // System.out.println(j);
+            int a = j / TEN;
+            while (a > 0) {
+              if (a % TEN == SEVEN)  {
+                value += 1;
+            }
+                a = a / TEN;
+            }
+            // System.out.println(n);
+            if (a == SEVEN){
+                value += 1;
+                // System.out.println(value);
+            }
 
-		}
-		int result = count + value ;
+        }
+        int result = count + value ;
         System.out.println(result);
     }
 }
