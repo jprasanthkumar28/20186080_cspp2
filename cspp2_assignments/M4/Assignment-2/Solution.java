a120ab95c349a4eabad18977c467214d30cbb2f7
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-30 14:59:36 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2_assignments/M4/Assignment-2/Solution.java
+++ cspp2_assignments/M4/Assignment-2/Solution.java
@@ -1,42 +1,64 @@
-import java.util.*;
-public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int m = s.nextInt();
-		int n = s.nextInt();
-		int[][] a = new int[m][n];
-		int[][] b = new int[m][n];
-		int[][] sum = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = s.nextInt();
-			}
-		}
-		int m1 = s.nextInt();
-		int n1 = s.nextInt();
-		for (int i = 0; i < m1; i++) {
-			for (int j = 0; j < n1; j++) {
-				b[i][j] = s.nextInt();
-			}
-		}
-		int i,j;
-		for (i = 0; i < m; i++) {
-			for (j = 0; j < n; j++) {
-				sum[i][j] = a[i][j] + b[i][j];
-				// System.out.print(sum[i][j] + " ");
-			}
-			// System.out.println(sum[i][j]);
-		}
-		for (i = 0; i <= m-1; i++) {
-			for (j = 0; j < n-1; j++) {
-				System.out.print(sum[i][j] + " ");
-			}
-			System.out.println(sum[i][j]);
-		}
-		// for (int i = 0; i < m; i++) {
-		// System.out.println(Arrays.toString(sum[i]));			
-		// }
+/**
+ *@author Prasanth...
+*/
+import java.util.Scanner;
+public final class Solution {
+    /**
+     * Empty Constructor.
+     */
+    // private static final int MAXVALUE = 5;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Iam not using this constructor
+    }
+    /**
+     * Main function to print.
+     * @param args is the parameter for this function
+    */
+    /**
+     * Fill the main function to print resultant of addition of matrices
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
-	}
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+        int[][] a = new int[m][n];
+        int[][] b = new int[m][n];
+        int[][] sum = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        int m1 = s.nextInt();
+        int n1 = s.nextInt();
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
+        int i, j;
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+                // System.out.print(sum[i][j] + " ");
+            }
+            // System.out.println(sum[i][j]);
+        }
+        for (i = 0; i <= m - 1; i++) {
+            for (j = 0; j < n - 1; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println(sum[i][j]);
+        }
+        // for (int i = 0; i < m; i++) {
+        // System.out.println(Arrays.toString(sum[i]));         
+        // }
+
+    }
 }
