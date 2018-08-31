b64ca6e99aa67ca38effcd71f6ac71fa771be1d0
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-31 16:15:41 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2_assignments/M4/Assignment-3/Solution.java
+++ cspp2_assignments/M4/Assignment-3/Solution.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /**
  *@author Prasanth...
 */
-public class Solution {
+public final class Solution {
     /**
     Do not modify the main function.
     */
@@ -16,27 +16,26 @@ public class Solution {
      *
      * @param      args  The arguments
      */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i = 0; i < n; i++) {
-			String s = sc.next();
-			long res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static long binaryToDecimal(final String value) {
-		double  result;
-		int sum = 0;
-		int p = value.length() - 1;
-		for (int i = 0; i < value.length(); i++) {
-			if (value.charAt(i) != '0') {
-				result = Math.pow(2, p);
-				sum += result;
-			}
-			p = p - 1;
-		}
-		return sum;
-	}
-
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            long res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    public static long binaryToDecimal(final String value) {
+        double  result;
+        int sum = 0;
+        int p = value.length() - 1;
+        for (int i = 0; i < value.length(); i++) {
+            if (value.charAt(i) != '0') {
+                result = Math.pow(2, p);
+                sum += result;
+            }
+            p = p - 1;
+        }
+        return sum;
+    }
 }
