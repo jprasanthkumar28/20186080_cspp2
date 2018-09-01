bf4b81775f1f1241b8be0494a3b9c22e21fb5068
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-09-01 12:52:33 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- cspp2_assignments/M6/Assignment-2/Solution.java
+++ cspp2_assignments/M6/Assignment-2/Solution.java
@@ -33,13 +33,6 @@ final class Solution {
                 else if ((a[i][j] >= 50) && (a[i][j]-100) <=149) {
                     a[i][j] = 100;
                 }
-                else if ((a[i][j] >= 200) && (a[i][j]-100) <=249) {
-                    a[i][j] = 200;
-                }
-                else if ((a[i][j] >= 300) && (a[i][j]-100) <=349) {
-                    a[i][j] = 300;
-                }
-
         }
     }return a;
 	
