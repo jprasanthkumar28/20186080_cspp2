15a90b29b3393421298aee2f707fdf4efc41c809
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-09-06 09:58:07 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2_assignments/M8/Homework_/List.java
+++ cspp2_assignments/M8/Homework_/List.java
@@ -240,6 +240,9 @@ public class List {
             str = str + list[i] + ",";
         }
         str = str + list[i] + "]";
+        if (get(0) == 0) {
+            str += "\nInvalid Position Exception";
+        }
         return str;
     }
     
