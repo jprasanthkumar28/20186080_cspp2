3237b7a2e3565e7de591d549c6f62676502150d8
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-09-07 17:35:06 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2_assignments/M11/Assignment-1 (7)/Assignment-1/List.java
+++ cspp2_assignments/M11/Assignment-1 (7)/Assignment-1/List.java
@@ -462,7 +462,8 @@ public final class List {
                     }
                 break;
                 case "subList": {
-                    if (tokens.length != 2) { break;
+                    if (tokens.length != 2) {
+                        break;
                     }
                     String[] arrstring3 = tokens[1].split(",");
                     List object = l.subList(Integer.parseInt(arrstring3[0]),
