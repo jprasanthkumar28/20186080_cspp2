6c99ce332a99d41ea8890e25d99b4a3e281939d8
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-09-22 11:20:32 +0530

    "check_style_score -> 4 of 14 passed. style score: 0/1 "

---
+++ cspp2_assignments/M25/Assignment-1/TodoistMain.java
@@ -0,0 +1,180 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+  * write your code below this comment
+  */
+class Task {
+	private String taskTitle;
+	private String name;
+	private int taskTime;
+	private boolean taskImportant;
+	private boolean taskUrgent;
+	private String taskStatus;
+	protected Task(String title, String assignedTo, int timeToComplete,
+		boolean important, boolean urgent, String status) {
+		this.taskTitle = title;
+		this.name = assignedTo;
+		this.taskTime = timeToComplete;
+		this.taskImportant = important;
+		this.taskUrgent = urgent;
+		this.taskStatus = status;
+	}
+	public String getImp() {
+		String str = "";
+		if (this.taskImportant == true) {
+			str = "Important";
+		} else {
+			str = "Not Important";
+		}
+		return str;
+	}
+	public String getUrgent() {
+		String str = "";
+		if (this.taskUrgent == true) {
+			str = "Urgent";
+		} else {
+			str = "Not Urgent";
+		}
+		return str;
+	}
+	public String toString() {
+		String str = "";
+		str = taskTitle + ", " + name + ", " + taskTime + ", " + getImp() + ", " +
+		getUrgent() + ", " + taskStatus;
+		return str;
+	}
+}
+class Todoist {
+	private Task[] taskdata;
+	private int tasksize;
+	protected Todoist() {
+		taskdata = new Task[10];
+		tasksize = 0;
+	}
+	public void addTask(Task task) {
+		if (tasksize == taskdata.length) {
+            resize();
+        }
+        else {
+            taskdata[tasksize++] = task;
+        }
+	}
+	public void resize() {
+		taskdata = Arrays.copyOf(taskdata, tasksize + 2);
+	}
+	public String toString() {
+		String str = "";
+		for (int i = 0; i < tasksize; i++) {
+			// System.out.println(taskdata[i]);
+			str += taskdata[i];
+		}
+		return str;
+	}
+	public Task getNextTask(String personName) {
+		return null;
+	}
+	public Task[] getNextTask(String personName, int taskcount) {
+		return null;
+	}
+	public int totalTime4Completion() {
+		return 0;
+	}
+
+}
+/**
+ * Class for todoist main.
+ */
+public class TodoistMain {
+
+    /**
+     * Starts a test.
+     */
+    public static void startTest() {
+        Todoist todo = new Todoist();
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+            String[] tokens = s.nextLine().split(",");
+            switch (tokens[0]) {
+                case "task":
+                    testTask(tokens);
+                break;
+                case "add-task":
+                    testAddTask(todo, tokens);
+                break;
+                case "print-todoist":
+                    System.out.println(todo);
+                break;
+                case "get-next":
+                    System.out.println(todo.getNextTask(tokens[1]));
+                break;
+                case "get-next-n":
+                    int n = Integer.parseInt(tokens[2]);
+                    Task[] tasks = todo.getNextTask(tokens[1], n);
+                    System.out.println(Arrays.deepToString(tasks));
+                break;
+                case "total-time":
+                    System.out.println(todo.totalTime4Completion());
+                break;
+                default:
+                break;
+            }
+        }
+    }
+
+    /**
+     * method to test add task.
+     *
+     * @param      todo    The todo
+     * @param      tokens  The tokens
+     */
+    public static void testAddTask(final Todoist todo, final String[] tokens) {
+        try {
+            todo.addTask(createTask(tokens));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * method to test the creation of task object.
+     *
+     * @param      tokens  The tokens
+     */
+    public static void testTask(final String[] tokens) {
+        try {
+            System.out.println(createTask(tokens));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Creates a task object.
+     *
+     * @param      tokens     The tokens
+     *
+     * @return     Task object
+     *
+     * @throws     Exception  if task inputs are invalid
+     */
+    public static Task createTask(final String[] tokens) throws Exception {
+        String title = tokens[1];
+        String assignedTo = tokens[2];
+        int timeToComplete = Integer.parseInt(tokens[3]);
+        boolean important = tokens[4].equals("y");
+        boolean urgent = tokens[5].equals("y");
+        String status = tokens[6];
+        return new Task(
+            title, assignedTo, timeToComplete, important, urgent, status);
+    }
+
+    /**
+     * main method.
+     *
+     * @param      args  The command line arguments
+     */
+    public static void main(final String[] args) {
+        startTest();
+    }
+}
